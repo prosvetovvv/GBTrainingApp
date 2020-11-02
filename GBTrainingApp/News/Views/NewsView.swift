17f135236afec92b33b0bdd7172b169414835254
prosvetovvv <vp12@yandex.ru>
2020-11-02 09:29:03 +0300

    add files

---
+++ GBTrainingApp/News/Views/NewsView.swift
@@ -0,0 +1,20 @@
+//
+//  NewsView.swift
+//  GBTrainingApp
+//
+//  Created by Vitaly Prosvetov on 02.11.2020.
+//
+
+import UIKit
+
+class NewsView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
